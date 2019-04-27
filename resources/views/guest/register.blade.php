17a426e52ed55536765ab593f7560cc855df3b1d
Clavianus Juneardo <juneardoc@gmail.com>
2019-04-27 22:32:25 +0700

    Fix register system

--- resources/views/guest/register.blade.php
+++ resources/views/guest/register.blade.php
@@ -6,14 +6,26 @@
   <div class="columns is-mobile is-centered">
     <h1 class="title">Register Form</h1>
   </div>
-  <form method="POST" action="/api/register">
+  <form method="POST" action="/api/register" onsubmit="cacheInput()">
+    <div class="columns is-mobile is-centered">
+      <div class="column is-one-third">
+        <div class="field">
+          <p class="control has-icons-left">
+            <input name="name" id="name" class="input" type="text" placeholder="Full Name" autofocus>
+            <span class="icon is-small is-left">
+              <i class="fa fa-user"></i>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
     <div class="columns is-mobile is-centered">
       <div class="column is-one-third">
         <div class="field">
           <p class="control has-icons-left">
             <input name="username" id="nis" class="input" type="text" placeholder="Student's Number" autofocus>
             <span class="icon is-small is-left">
-              <i class="fa fa-user"></i>
+              <i class="fa fa-address-card"></i>
             </span>
           </p>
         </div>
@@ -35,7 +47,7 @@
       <div class="column is-one-third">
         <div class="field">
           <p class="control has-icons-left">
-            <input name="confirm-password" id="confirm-password" class="input" type="password" placeholder="Confirm Password">
+            <input name="password_confirmation" id="confirm-password" class="input" type="password" placeholder="Confirm Password">
             <span class="icon is-small is-left">
               <i class="fa fa-lock"></i>
             </span>
@@ -48,13 +60,13 @@
         <div class="field">
           <div class="control has-icons-left">
             <div class="select is-fullwidth">
-              <select id="select-major">
-                <option value=0 selected>Major</option>
-                <option value='Teacher'>Teacher</option>
-                <option value='AK'>AK</option>
-                <option value='MM'>MM</option>
-                <option value='AP'>AP</option>
-                <option value='TKJ'>TKJ</option>
+              <select name="major" id="select-major">
+                <option value="null" selected>Major</option>
+                <option value="Teacher">Teacher</option>
+                <option value="AK">AK</option>
+                <option value="MM">MM</option>
+                <option value="AP">AP</option>
+                <option value="TKJ">TKJ</option>
               </select>
             </div>
             <div class="icon is-small is-left">
@@ -69,11 +81,11 @@
         <div class="field is-grouped">
           <div class="control is-expanded has-icons-left">
             <div class="select is-fullwidth">
-              <select id="select-class">
-                <option value=0 selected>Class</option>
-                <option value=10>X</option>
-                <option value=11>XI</option>
-                <option value=12>XII</option>
+              <select name="class" id="select-class">
+                <option value="null" selected>Class</option>
+                <option value="10">X</option>
+                <option value="11">XI</option>
+                <option value="12">XII</option>
               </select>
             </div>
             <div class="icon is-small is-left">
@@ -82,18 +94,18 @@
           </div>
           <div class="control is-expanded has-icons-left">
             <div class="select is-fullwidth">
-              <select id="select-grade">
-                <option value=0 selected>Grade</option>
-                <option value=1>1</option>
-                <option value=2>2</option>
-                <option value=3>3</option>
-                <option value=4>4</option>
-                <option value=5>5</option>
-                <option value=6>6</option>
-                <option value=7>7</option>
-                <option value=8>8</option>
-                <option value=9>9</option>
-                <option value=10>10</option>
+              <select name="grade" id="select-grade">
+                <option value="null" selected>Grade</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
               </select>
             </div>
             <div class="icon is-small is-left">
@@ -113,24 +125,38 @@
 @endsection
 
 @section('script')
-@if ($errors->any())
 <script type="text/javascript">
-  toggleModal('red', 'Authentication Error', 'Wrong Username / Password')
-</script>
-@else
+  const cacheInput = () => {
+    store.set('name', $('#name').value)
+    store.set('nis', $('#nis').value)
+    store.set('major', $('#select-major').value)
+    store.set('grade', $('#select-grade').value)
+    store.set('class', $('#select-class').value)
 
-<script type="text/javascript">
-  $('#select-major').addEventListener('click', () => {
-    if($('#select-major').value === 'Teacher'){
+  }
 
-      $('#select-class').setAttribute('disabled', 'disabled')
-      $('#select-grade').setAttribute('disabled', 'disabled')
-    }
-    else{
-      $('#select-class').removeAttribute('disabled')
-      $('#select-grade').removeAttribute('disabled')
-    }
+  const validateMajor = () => {
+    let isDisable = $('#select-major').value === 'Teacher' ? true : false
+    $('#select-class').disabled = isDisable
+    $('#select-grade').disabled = isDisable
+  }
+
+  window.addEventListener('load', () => {
+    $('#name').value = store.get('name') || ''
+    $('#nis').value = store.get('nis') || ''
+    $('#select-major').value = store.get('major')
+    $('#select-grade').value = store.get('grade')
+    $('#select-class').value = store.get('class')
+    validateMajor()
   })
+
+  $('#select-major').addEventListener('change', validateMajor)
+
+  @if (Session::has('success'))
+    toggleModal('green', 'Register Success', '{{ Session::get('success') }}')
+    store.clear()
+  @elseif ($errors->any())
+    toggleModal('red', 'Register Error', '{{ $errors->first() }}')
+  @endif
 </script>
-@endif
 @endsection
