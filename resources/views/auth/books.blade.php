9b646179aed0029a03a13c3519f7d1e0763d9539
munyaaa <jessicajimantoro@gmail.com>
2019-04-28 20:36:14 +0700

    add-search-bar

--- resources/views/auth/books.blade.php
+++ resources/views/auth/books.blade.php
@@ -3,16 +3,64 @@
 
 @section('content')
 <main class="container has-text-centered">
-  <h1 class="title">Coming Soon</h1>
+  <div class="field">
+    <div class="control has-icons-left search-bar">
+      <input class="input is-large" type="text" placeholder="Search">
+      <span class="icon is-medium is-left">
+        <i class="fas fa-search"></i>
+      </span>
+    </div>
+  </div>
 </main>
 @endsection
 
 @section('script')
 <script type="text/javascript">
-  window.addEventListener('load', async () => {
-    await getBooks(res => {
-      console.log(res)
+  window.addEventListener('load', () => {
+    req.get('/api/books').then(res => {
+      console.log(store.get('books_on_held'))
+      $('main.container').innerHTML += `
+        <div class="tile is-ancestor anc-div first-anc">
+        <div class="tile is-parent box">
+        <div class="tile is-child">
+        <div class="title is-5">Title</div>
+        </div>
+        <div class="tile is-child">
+        <div class="title is-5">Author</div>
+        </div>
+        <div class="tile is-child">
+        <div class="title is-5">Stock</div>
+        </div>
+        <div class="tile is-child">
+        <div class="title is-5"></div>
+        </div>
+        </div>
+        </div
+        `
+      res.forEach(child => {
+
+        console.log(child)
+        $('main.container').innerHTML += `
+        <div class="tile is-ancestor anc-div">
+        <div class="tile is-parent box">
+        <div class="tile is-child">
+        <div class="subtitle">${child.title}</div>
+        </div>
+        <div class="tile is-child">
+        <div class="subtitle">${child.author}</div>
+        </div>
+        <div class="tile is-child">
+        <div class="subtitle">${child.stock}</div>
+        </div>
+        <div class="tile is-child">
+        <span class="button is-success is-fullwidth req-btn" disabled="${store.get('books_on_held') == 2 ? true : false }">Request</span>
+        </div>
+        </div>
+        </div>
+        `
+      })
     })
   })
 </script>
+
 @endsection
