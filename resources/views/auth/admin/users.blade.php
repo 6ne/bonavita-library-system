67b4451cd802c1017fc0b2ae89c370a6bcdf45a9
munyaaa <jessicajimantoro@gmail.com>
2019-05-03 02:36:35 +0700

    user-page-1

--- resources/views/auth/admin/users.blade.php
+++ resources/views/auth/admin/users.blade.php
@@ -1,46 +1,60 @@
 @extends('layouts.app')
 @section('title', 'Users')
 
+@section('style')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/users-page.css') }}">
+@endsection
+
 @section('content')
 <main class="container has-text-centered">
   <div class="columns">
     <div class="column">
-      <div class="columns box">
-        <div class="column">
-          <span>TKJ</span>
+      <div class="columns message user-card box">
+        <div class="column left-side-card">
+          <div class="major">TKJ</div>
+          <div class="class">12-2</div>
         </div>
         <div class="column">
-          <div class="columns">
-            <div class="column">
-              <div>ID</div>
-              <div>Name</div>
-            </div>
-            <div class="column">
-              <div>2011219829</div>
-              <div>Clavin June</div>
-            </div>
+          <div>2101651035</div>
+          <div class="name">Clavin June</div>
+          <div>Books: <span class="tag is-danger">2</span></div>
+          <div class="detail-button button is-primary is-fullwidth is-rounded">
+            <span class="icon is-small"><i class="fas fa-list-ul"></i></span>
+            <span>Details</span>
           </div>
         </div>
       </div>
     </div>
     <div class="column">
-      <div class="columns box">
+      <div class="columns message user-card box">
         <div class="column">
-          <span>MM</span>
+          <div>MM</div>
+          <div>11-1</div>
         </div>
         <div class="column">
           <div>2101651035</div>
           <div>Jessica Tania J</div>
+          <div>Books on held : <span>2</span></div>
+          <div class="detail-button button is-link is-fullwidth is-rounded">
+            <span class="icon is-small"><i class="fas fa-list-ul"></i></span>
+            <span>Details</span>
+          </div>
         </div>
       </div>
     </div>
     <div class="column">
-      <div class="columns box">
+      <div class="columns message user-card box">
         <div class="column">
-          <span>TCR</span>
+          <div>TCR</div>
         </div>
         <div class="column">
-          <span>Markidi</span>
+          <div>2101651035</div>
+          <div>Markidi</div>
+          <div>Books on held : <span>2</span></div>
+          <div class="detail-button button is-link is-fullwidth is-rounded">
+            <span class="icon is-small"><i class="fas fa-list-ul"></i></span>
+            <span>Details</span>
+          </div>
         </div>
       </div>
     </div>
